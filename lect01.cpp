726a8317c5b9d03019c57412ed13c4574a4b5582
Mansee <silochanaasati30@gmail.com>
2025-11-16 09:17:04 +0530

    lect01

---
+++ lect01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"DSA by apna college"<<endl;
+    // type conversion (implecit)
+    char a='m';
+    int b=a;
+    cout<<a<<"\n"<<b<<"\n";
+    // type casting
+    double price=100.99;
+    int newprice=(int) price;
+    cout<<price<<"\n"<<newprice<<"\n";
+    return 0;
+}
